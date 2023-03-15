5cb259a14c1c4b48383e4a5788cfdcc61b563b20
kusupon <kusupon0628@gmail.com>
2023-03-15 16:37:46 +0900

    first commit

---
+++ src/components/Form.tsx
@@ -0,0 +1,18 @@
+type FormPropsType = {
+    city: string;
+    setCity: React.Dispatch<React.SetStateAction<string>>;
+    getWeather: (e: any) => void;
+}
+
+const Form = ({ setCity, getWeather, city }: FormPropsType) => {
+    return (
+        <>
+        <form onSubmit={getWeather}>
+            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} value={city} />
+            <button type="submit">Get Weather</button>
+        </form>
+        </>
+    )
+}
+
+export default Form;
---
+++ src/components/Results.tsx
@@ -0,0 +1,27 @@
+type ResultsPropsType = {
+    results: {
+        country: string
+        cityName: string
+        temperature: string
+        conditionText: string
+        icon: string
+    }
+}
+
+const Results = ({ results }: ResultsPropsType) => {
+    const { country, cityName, temperature, conditionText, icon } = results;
+    return (
+        <>
+            {country && <div>{country}</div>}
+            {cityName && <div>{cityName}</div>}
+            {temperature && <div>{temperature}</div>}
+            {conditionText &&
+                <div>
+                    <img src={icon} alt="icon" />
+                    <span>{conditionText}</span>
+                </div>}
+        </>
+    );
+};
+
+export default Results;
